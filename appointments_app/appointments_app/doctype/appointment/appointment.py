05f17631d594178330adec316c7ce935b61749b0
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-07-12 17:11:37 +0530

    feat: basic appointment queue

--- appointments_app/appointments_app/doctype/appointment/appointment.py
+++ appointments_app/appointments_app/doctype/appointment/appointment.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2023, Build With Hussain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Appointment(Document):
-	pass
+	def after_insert(self):
+		print("your queue number: ", self.add_to_appointment_queue())
+
+	def add_to_appointment_queue(self):
+		q = frappe.get_doc(
+			"Appointment Queue",
+			{
+				"date": self.date,
+				"shift": self.shift,
+				"clinic": self.clinic,
+			},
+		)
+		q.append("queue", {"appointment": self.name, "status": "Pending"})
+		q.save(ignore_permissions=True)
+
+		return len(q.queue)
