563301cf8f312166060dff87921bb41ab94ac9a6
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-07-12 16:11:59 +0530

    refactor: better shift management

--- appointments_app/appointments_app/doctype/appointment/appointment.js
+++ appointments_app/appointments_app/doctype/appointment/appointment.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2023, Build With Hussain and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Appointment", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Appointment", {
+	refresh(frm) {
+        frm.set_query('shift', (doc) => {
+            return {
+                filters: {
+                    "clinic": doc.clinic
+                }
+            }
+        })
+	},
+});
--- appointments_app/appointments_app/doctype/schedule_shift/schedule_shift.py
+++ appointments_app/appointments_app/doctype/schedule_shift/schedule_shift.py
@@ -3,7 +3,12 @@
 
 # import frappe
 from frappe.model.document import Document
+from frappe.utils import format_time
 
 
 class ScheduleShift(Document):
-	pass
+	def before_save(self):
+		# set the title field to "start_time-end_time"
+		self.title = (
+			format_time(self.start_time, "HH:mm") + "-" + format_time(self.end_time, "HH:mm")
+		)
