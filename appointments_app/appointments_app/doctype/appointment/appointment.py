0f28dd1065108a58d1522b75bfdd46d314180c08
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-07-15 21:57:29 +0530

    fix: handle case when appointment queue doesn't exist

--- appointments_app/appointments_app/doctype/appointment/appointment.py
+++ appointments_app/appointments_app/doctype/appointment/appointment.py
@@ -7,17 +7,27 @@ from frappe.model.document import Document
 
 class Appointment(Document):
 	def after_insert(self):
-		print("your queue number: ", self.add_to_appointment_queue())
+		self.queue_number = self.add_to_appointment_queue()
+		self.save(ignore_permissions=True)
 
 	def add_to_appointment_queue(self):
-		q = frappe.get_doc(
-			"Appointment Queue",
-			{
+		filters = {
 				"date": self.date,
 				"shift": self.shift,
 				"clinic": self.clinic,
-			},
+			}
+		appointment_queue_exists = frappe.db.exists(
+			"Appointment Queue",
+			filters,
 		)
+
+		if appointment_queue_exists:
+			q = frappe.get_doc("Appointment Queue", filters)
+		else:
+			q = frappe.new_doc("Appointment Queue")
+			q.update(filters)
+			q.save(ignore_permissions=True)
+	
 		q.append("queue", {"appointment": self.name, "status": "Pending"})
 		q.save(ignore_permissions=True)
 
--- appointments_app/appointments_app/doctype/appointment/test_appointment.py
+++ appointments_app/appointments_app/doctype/appointment/test_appointment.py
@@ -1,9 +1,37 @@
 # Copyright (c) 2023, Build With Hussain and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestAppointment(FrappeTestCase):
-	pass
+	def test_add_to_appointment_queue(self):
+		doctor = frappe.get_doc({"doctype": "Doctor", "first_name": "Test Doctor", "speciality": "Pediatrician"}).insert()
+		clinic = frappe.get_doc({"doctype": "Clinic", "name": "Test Clinic", "doctor": doctor.name, "contact_number": "+918770773631"}).insert()
+		shift = frappe.get_doc({"doctype": "Schedule Shift", "start_time": "9:00:00", "end_time": "15:00:00", "clinic": clinic.name}).insert()
+
+		day = '2023-07-12'
+
+		appointment = frappe.get_doc({
+			"doctype": "Appointment",
+			"clinic": clinic.name,
+			"shift": shift.name,
+			"date": day,
+			"patient_name": "Test Patient",
+		}).insert()
+
+		self.assertEqual(appointment.queue_number, 1)
+
+		appointment = frappe.get_doc({
+			"doctype": "Appointment",
+			"clinic": clinic.name,
+			"shift": shift.name,
+			"date": day,
+			"patient_name": "Test Patient",
+		}).insert()
+
+		self.assertEqual(appointment.queue_number, 2)
+		
+	def tearDown(self):
+		frappe.db.rollback()
