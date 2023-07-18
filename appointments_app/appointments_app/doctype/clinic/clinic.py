127d2aba847a19be6c74fafe942cd781c615f833
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-07-18 10:00:12 +0530

    feat: realtime doctor IN/OUT status update!

--- appointments_app/appointments_app/doctype/clinic/clinic.py
+++ appointments_app/appointments_app/doctype/clinic/clinic.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2023, Build With Hussain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class Clinic(WebsiteGenerator):
-	pass
+	def on_update(self):
+		if self.has_value_changed("doctor_in"):
+			frappe.publish_realtime(
+				"doctor_status_changed", {"updated_status": "IN" if self.doctor_in else "OUT", "clinic": self.name}
+			)
