5f724c13cca22c41c7a4fb2ea962b83c360c2c10
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-08-02 22:48:43 +0530

    chore: better sms message

--- appointments_app/appointments_app/doctype/appointment/appointment.py
+++ appointments_app/appointments_app/doctype/appointment/appointment.py
@@ -7,7 +7,7 @@ from frappe.model.document import Document
 
 class Appointment(Document):
 	def validate(self):
-		# validate the contact_number to be a valid 10 digit phone number except the country code, 
+		# validate the contact_number to be a valid 10 digit phone number except the country code,
 		# if it does not have a country code, prepend +91
 		if not self.contact_number:
 			frappe.throw("Please enter a valid contact number")
@@ -17,7 +17,7 @@ class Appointment(Document):
 			pass
 		else:
 			frappe.throw("Please enter a valid contact number")
-		
+
 	def after_insert(self):
 		self.queue_number = self.add_to_appointment_queue()
 		# attach csrf token + queue number as key and queue number as value
@@ -49,9 +49,12 @@ class Appointment(Document):
 		return len(q.queue)
 
 	def send_confirmation_message(self):
+		shift_title = frappe.db.get_value("Schedule Shift", self.shift, "title")
+		message = f"Hi {self.patient_name}, your appointment for {self.clinic} on {self.date} ({shift_title}) has been booked. Your queue number is {self.queue_number}."
+
 		frappe.enqueue(
 			"appointments_app.utils.send_message",
-			body=f"Appointment Booked! Your queue number is {self.queue_number} at {self.clinic} on {self.date}",
+			body=message,
 			from_=frappe.db.get_single_value(
 				"Appointments Twilio Settings", "from_phone_number"
 			),
