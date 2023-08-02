74070d6f922c62a3ba0403f374510ab570ab1649
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-08-02 23:13:45 +0530

    chore: set website footer

--- appointments_app/install.py
+++ appointments_app/install.py
@@ -3,3 +3,4 @@ import frappe
 
 def after_install():
 	frappe.db.set_single_value("Website Settings", "website_theme", "JDP Doctors")
+	frappe.db.set_single_value("Website Settings", "address", "Made with 💖 in Jagdalpur, by Hussain")
